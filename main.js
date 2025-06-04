de1fc63713045c96415af99030287069853902af
Benson Williams <anthonybensonba837@gmail.com>
2025-06-03 22:43:25 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,44 +1,112 @@
-function isEvenlyDivisible() {
-
+function isEvenlyDivisible(a, b) {
+  return a % b === 0;
 }
 
-function halfSquare() {
+function halfSquare(num) {
+  return (num ** 2) / 2;
 }
 
-function isLong() {
+function isLong(str) {
+  return str.length >= 15;
 }
 
-function exclaim() {
+function exclaim(str) {
+  let result = '';
+  let end = str.length - 1;
+  while (end >= 0 && str[end] === '!'){
+    end--;
+  }
+  for(let i = 0; i <= end; i++){
+    result += str[i];
+  }
+  return result + '!';
 }
 
-function countWords() {
+function countWords(str) {
+  let words = str.trim().split(' ');
+  return words.filter(word => word !== '').length;
 }
 
-function containsDigit() {
+function containsDigit(str) {
+  let digits = '1234';
+  for (let i = 0; i < str.length; i++) {
+    if (digits.includes(str[i])) {
+      return true;
+    }
+  } return false;
 }
 
-function containsLowerCase() {
+function containsLowerCase(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char >= 'a' && char <= 'z') {
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsUpperCase() {
+function containsUpperCase(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char >= 'A' && char <= 'Z') {
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsNonAlphanumeric() {
+function containsNonAlphanumeric(str) {
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (!(char >= 'a' && char <= 'z') &&
+      !(char >= 'A' && char <= 'Z') &&
+      !(char >= '0' && char <= '9')) {
+      return true;
+    }
+  }
+  return false;
 }
 
-function containsSpace() {
+function containsSpace(str) {
+  for(let i = 0; i < str.length; i++){
+    let spaces = str[i];
+    if(spaces === ' '){
+      return true;
+    }
+  }
+  return false;
 }
 
-function digits() {
+function digits(num) {
+  let str = "" + num;
+  let result = [];
+  for(let i = 0; i < str.length; i++){
+    let char = str[i];
+    if(char >= '0' && char <= '9'){
+      result.push(Number(char));
+    }
+  }
+  
+  return result;
 }
 
-function truncate() {
+function truncate(str) {
+  if(str.length < 15) {
+    return str;
+  }
+
+  let result = '';
+  for(let i = 0; i < 8; i++){
+    result += str[i];
+  }
+  return result + '...';
 }
 
 function isValidPassword() {
 }
 
-function onlyPunchy(arr) {
+function onlyPunchy() {
 }
 
 
